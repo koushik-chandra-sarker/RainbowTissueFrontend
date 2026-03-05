b5182fdd21a99df6218956ff2886f748b07c6bdc
Koushik Chandra Sarker <koushik.chandra@bracits.com>
2026-03-05 22:08:14 +0600

    fix classname issue

--- pages/shop/product/[id].js
+++ pages/shop/product/[id].js
@@ -509,7 +509,7 @@ const Product = (props) => {
                                             <h2 className={'text-sm border-b border-gray-200 font-roboto text-gray-800 pb-3 font-medium'}>
                                                 Product Reviews
                                             </h2>
-                                            <section classNauthame={'w-full'}>
+                                            <section className={'w-full'}>
                                                 {/*comment*/}
                                                 {
                                                     auth ?
